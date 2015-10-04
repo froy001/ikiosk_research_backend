3c0a36645a584768e4c98871ee64078e4d437815
froy001 <davidfradis@gmail.com>
2015-10-04 13:24:46 +0300

    completed addding mailer NO TESTS!!!

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,9 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+    user = User.find_or_create_by!(email: Figaro.env.admin_email) do |user|
+        user.password = Figaro.env.admin_password
+        user.password_confirmation = Figaro.env.admin_password
+        user.name = Figaro.env.admin_name
         user.admin!
       end
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,11 +30,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: Figaro.env.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: Figaro.env.GMAIL_USERNAME,
+    password: Figaro.env.GMAIL_PASSWORD
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
