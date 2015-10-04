44bc3bc41b238af6d3bbc027af79e3ebdcf26fa4
froy001 <davidfradis@gmail.com>
2015-10-04 10:48:07 +0300

    added geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921115528) do
+ActiveRecord::Schema.define(version: 20150921132505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150921115528) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
