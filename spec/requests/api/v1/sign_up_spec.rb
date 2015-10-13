03a03991a3fc1ce0bb50f74cd1df29058e358d3c
froy001 <davidfradis@gmail.com>
2015-10-14 00:10:51 +0300

    refactored api sign_up spec

--- spec/requests/api/v1/sign_up_spec.rb
+++ spec/requests/api/v1/sign_up_spec.rb
@@ -1,11 +1,31 @@
 describe 'Sign Up API' do
-  context 'when unregistered user signs up' do
+  context 'when user signs up' do
     let(:sign_up) { sign_up_with_api(email, pass, pass_confirmation) }
     before(:each) do
       sign_up
     end
+
+    context 'valid credentials' do
+      let(:email) { "froy001@gmail.com" }
+      let(:pass) { '12345678' }
+      let(:pass_confirmation) { '12345678' }
+
+      it 'responds with success and code 200' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns a user json with field id not nil' do
+        expect(json['data']['id']).not_to be_nil
+      end
+
+      it 'has email = uid' do
+        expect(json['data']['email']).to eq(json['data']['email'])
+      end
+    end
+
     context 'invalid credentials' do
-      context 'when invalid email format' do
+      context 'invalid email' do
         let(:email) { "froy001" }
         let(:pass) { '12345678' }
         let(:pass_confirmation) { pass }
@@ -28,7 +48,7 @@ describe 'Sign Up API' do
         end
       end
 
-      context 'with pass != pass_confirmation' do
+      context 'pass != pass_confirmation' do
         let(:email) { "froy001@gmail.com" }
         let(:pass) { '12345678' }
         let(:pass_confirmation) { '1' }
@@ -51,7 +71,7 @@ describe 'Sign Up API' do
         end
       end
 
-      context 'with to short password' do
+      context 'password to short' do
         let(:email) { "froy001@gmail.com" }
         let(:pass) { '1234' }
         let(:pass_confirmation) { '1234' }
@@ -94,32 +114,12 @@ describe 'Sign Up API' do
           expect(json['errors']['email']).not_to be_blank
         end
 
-        it 'has 1 error: email: is not an email' do
+        it 'has 1 error: email: address is already in use' do
           sign_up_with_api(email, other_pass, other_pass)
           expect(json['errors'].to_json).to have_json_size(1).at_path("email")
           expect(json['errors']['email'][0]).to eq("address is already in use")
         end
       end
     end
-
-    context 'when valid credentials' do
-      let(:email) { "froy001@gmail.com" }
-      let(:pass) { '12345678' }
-      let(:pass_confirmation) { '12345678' }
-
-      it 'responds with success and code 200' do
-        expect(response).to be_success
-        expect(response.status).to eq(200)
-      end
-
-      it 'returns a user json with field id not nil' do
-        expect(json['data']['id']).not_to be_nil
-      end
-
-      it 'has email = uid' do
-        expect(json['data']['email']).to eq(json['data']['email'])
-      end
-    end
   end
-
 end
